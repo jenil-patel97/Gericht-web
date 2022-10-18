ec09432c1fd9c9b1a323980507e9ceb01fa4615c
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-18 12:27:13 +0530

    feat: IntroVideo

--- src/container/Intro.jsx
+++ src/container/Intro.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../constants";
+
+const Intro = () => {
+  const [play, setPlay] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlay((prevPlayVideo) => !prevPlayVideo);
+
+    if (play) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="h-screen relative">
+      <video
+        className="w-[100%] h-[100%] object-cover"
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+
+      <div className="absolute inset-0 bg-[rgba(0,0,0,.65)] flex justify-center items-center">
+        <div
+          onClick={handleVideo}
+          className="cursor-pointer border-[1px] border-[#e2c55a] w-[100px] h-[100px] rounded-[50%]"
+        >
+          {play ? (
+            <BsPauseFill
+              size={45}
+              color="white"
+              className="ml-[26px] mt-[25px]"
+            />
+          ) : (
+            <BsFillPlayFill
+              color="white"
+              className="ml-[26px] mt-[25px]"
+              size={45}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
